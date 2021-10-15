7217eee418f8dfdc742c2df732c9b4f6ad6f267a
Eli Hopkins <ewh3157@rit.edu>
2021-10-15 19:12:28 -0400

    Complete Sec Report and Copy Lab2LoadTest into Lab 3

---
+++ Lab3/Sec/Lab3LoadTest.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+###########################################
+# Config Crew Sec Team Loadtesting Script #
+###########################################
+
+# This script is intended to be used to test various loads on the Explore California Website.
+# To execute the script simply run ./Lab3LoadTest.sh from the Directory the Script is in.
+# The script may be run as any user and should only need access to Siege.
+
+# Get the IP Address to Test
+read -p $'Please enter \e[34mIP Address\e[0m to Test: ' IP
+
+# Check if Siege is Installed and Exit if not
+if ! which siege
+then
+    printf "Siege is not installed! Try running \e[35msudo yum install siege\e[0m"
+    exit 1
+fi
+
+# Main Loop
+while true
+do
+
+    # Ask the User for a Load Code
+    read -p $'Please select a load (M for Minimal, E for Expected, O for Overload, or Q for Quit): ' choice
+
+    # Exit if the User Chose to Quit
+    if [[ ${choice^^} == "Q" ]]
+    then
+        exit 0
+    fi
+
+    # Delay should Remain Constant for all Loads, 5 Seconds
+    # MINIMAL LOAD: Run Siege with 50 Concurrent Users, 100 Reps
+    if [[ ${choice^^} == "M" ]]
+    then
+        printf "Running Siege with \e[31m50\e[0m Concurrent Users, \e[35m15\e[0m Repetitions, and Under \e[34m5\e[0m Second Delay..."
+        sleep 3
+	siege -c 50 -r 15 -d 5 $IP
+    # EXPECTED LOAD: Run Siege with 100 Concurrent Users, 100 Reps
+    elif [[ ${choice^^} == "E" ]]
+    then
+        printf "Running Siege with \e[31m100\e[0m Concurrent Users, \e[35m15\e[0m Repetitions, and Under \e[34m5\e[0m Second Delay..."
+        sleep 3
+	siege -c 100 -r 15 -d 5 $IP
+    # Less Reps for Overload to Keep Script from Taking too Long
+    # OVER LOAD: Run Siege with 500 Concurrent Users, 50 Reps
+    elif [[ ${choice^^} == "O" ]]
+    then
+        printf "Running Siege with \e[31m255\e[0m Concurrent Users, \e[35m5\e[0m Repetitions, and Under \e[34m5\e[0m Second Delay..."
+        sleep 3
+	siege -c 255 -r 10 -d 5 $IP
+    # User Entered Invalid Code
+    else
+        echo "Invalid Load Choice!"
+    fi
+done
